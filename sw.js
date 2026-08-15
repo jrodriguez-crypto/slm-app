81e300263573b7688324fb5f1571512639f5d25b
jrodriguez-crypto <jrodriguez@slmabogados.com>
2026-08-15 15:23:11 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='slm-v1';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>new Response('')));});
